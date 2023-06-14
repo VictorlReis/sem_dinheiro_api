af6defef9697dea4dbcf066a4fcb832892131ce9
Victor Reis <victorsireis@gmail.com>
2023-06-14 18:07:16 -0300

    feat: adding transaction

--- src/main.py
+++ src/main.py
@@ -1,7 +1,41 @@
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+from src.models.transaction import Transaction
+from src.models.transaction import TransactionCreate
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+register_tortoise(
+    app,
+    db_url='sqlite://db.sqlite3',
+    modules={'models': ['src.models']},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
+
+
+@app.get("/transactions/{user_id}/{year}/{month}")
+async def get_transactions(user_id: str, year: int, month: int):
+    return await Transaction.filter(user_id=user_id, start_date__year=year, start_date__month=month)
+
+
+@app.post("/transaction")
+async def create_transaction(transaction: TransactionCreate):
+    new_transaction = await Transaction.create(**transaction.dict())
+    return new_transaction
+
+
+@app.put("/transaction/{transaction_id}")
+async def update_transaction(transaction_id: int, transaction: TransactionCreate):
+    transaction_db = await Transaction.get(id=transaction_id)
+    for key, value in transaction.dict().items():
+        setattr(transaction_db, key, value)
+    await transaction_db.save()
+    return transaction_db
+
+
+@app.delete("/transaction/{transaction_id}")
+async def delete_transaction(transaction_id: int):
+    transaction = await Transaction.get(id=transaction_id)
+    await transaction.delete()
+    return {"message": "Transaction deleted"}
--- src/models/transaction.py
+++ src/models/transaction.py
@@ -1,20 +1,32 @@
 from enum import Enum
 from typing import Optional
 from datetime import datetime
+from tortoise.models import Model
 from pydantic import BaseModel
 
 
-class TransactionType(str, Enum):
-    Expense = "Expense"
-    Income = "Income"
+class TransactionType(int, Enum):
+    Expense = 0
+    Income = 1
 
 
-class Transaction(BaseModel):
+class Transaction(Model):
     id: int
     description: str
     type: TransactionType
     start_date: datetime
     end_date: Optional[datetime] = None
+    payment_method: str
+    tag: str
+    value: float
+    user_id: str
+
+
+class TransactionCreate(BaseModel):
+    description: str
+    type: TransactionType
+    start_date: datetime
+    end_date: Optional[datetime]
     payment_method: str
     tag: str
     value: float
